c92b023d72998660ebb4da7e84cf8bcf4432c51e
dickyvisa <dickyvisa@gmail.com>
2025-09-20 02:22:30 +0700

    Batch replace: "dickypagesdev" -> "dickyvisa" | getAutoUpdateAllJuzMur.js | hits=1

--- functions/api/getAutoUpdateAllJuzMur.js
+++ functions/api/getAutoUpdateAllJuzMur.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-const GITHUB_REPO = "dickypagesdev/server";
+const GITHUB_REPO = "dickyvisa/server";
 const FILE_PATH   = "autoUpdateAllJuzMur.json"; // khusus Murajaah
 const BRANCH      = "main";
 
